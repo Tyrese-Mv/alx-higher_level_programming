7a5335b5b4413d8e0e4c13fdee65a40d0f20832a
Tyrese-Mv <musawenkosi106@gmail.com>
2023-08-16 08:56:19 -0700

    added task 0 fix 1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    for row in new_matrix:
-        for element in row:
-            row[element] = map(lambda x: x**2)
+    new_matrix = [row[:] for row in matrix]
+    for row in range(len(new_matrix)):
+        for element in range(len(new_matrix[row])):
+            new_matrix[row][element] = new_matrix[row][element] ** 2
     return new_matrix
