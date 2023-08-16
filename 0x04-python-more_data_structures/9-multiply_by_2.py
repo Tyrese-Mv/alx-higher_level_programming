86fc05a99635f6978a0031294894fc5c69a678c3
Tyrese-Mv <musawenkosi106@gmail.com>
2023-08-16 13:11:03 -0700

    added task 9 fix 1

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dictionary = {}
     for x, y in a_dictionary.items():
-        a_dictionary[x] = y * 2
-    return a_dictionary
+        new_dictionary[x] = y * 2
+    return new_dictionary
