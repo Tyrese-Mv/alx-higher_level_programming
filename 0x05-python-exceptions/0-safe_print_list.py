094b967e505d6074e9c61fbfbf627c7cbbd6c336
Tyrese-Mv <musawenkosi106@gmail.com>
2023-08-28 15:06:51 +0200

    added task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
+    except IndexError:
+        for item in my_list:
+            print("{}".format(item))
