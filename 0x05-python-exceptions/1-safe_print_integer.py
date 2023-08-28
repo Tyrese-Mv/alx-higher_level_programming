d7a7aa5739f00f8e2ae76c3cad6b357ad4405486
Tyrese-Mv <musawenkosi106@gmail.com>
2023-08-28 16:39:53 +0200

    added task 1 fix 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    ifHasNumber = True
     try:
-        for character in value:
-            if ord(character) >= 48 and ord(character) <= 57:
-                pass
-            else:
-                ifHasNumber = False
-        if ifHasNumber:
-            return ifHasNumber
+        print("{:d}".format(value)
+        return True
     except:
-        pass
-    print("{:d}".format(value))
-    return ifHasNumber
+        return False
