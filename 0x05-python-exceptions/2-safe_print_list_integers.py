e3a3be65bb9bd0ece2f73144fd3bfdc745169da3
Tyrese-Mv <musawenkosi106@gmail.com>
2023-08-28 17:13:54 +0200

    added task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for item in my_list:
+            if count < x:
+                if isinstance(item, int):
+                    print("{:d}".format(item), end="")
+                    count += 1
+    except IndexError as idx:
+        print(idx)
+    return count
