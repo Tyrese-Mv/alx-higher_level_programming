3983b46a30cea1abc52d04915198b7385ab63a55
Tyrese-Mv <musawenkosi106@gmail.com>
2023-08-29 21:20:43 +0200

    added task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """initialising the class attributes
+    Args:
+        size: initialise size.
+    """
+    def __init__(self, size=0):
+        self.__size = size
+    """
+    Getter
+    Arg:
+        self: self
+    """
+    def size(self):
+        return self.__size
+    """
+    Setter
+    Arg:
+        self: self
+    """
+    def size(self, value):
+        if int(value) >= 0:
+            self.__size = value
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        else:
+            raise ValueError("size must be >= 0")
+    """
+    method that returns current square area
+    Arg:
+        self: the size of the instatiated square
+    """
+    def area(self):
+        return self.__size*self.__size
