c66d038c8bb21d3ce60ee831cf444bd2c90a88fd
Tyrese-Mv <musawenkosi106@gmail.com>
2023-10-16 03:27:42 -0700

    added task 8 fix 2

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         num = ord(i)
         if num >= 97 and num <= 122:
             num -= 32
-        print("{}".format(chr(num)),end="")
+        print("{}".format(chr(num)), end="")
     print()
