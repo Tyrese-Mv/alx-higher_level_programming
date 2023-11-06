cc30058898443da2e78ca70f4125c662f918606d
Tyrese-Mv <musawenkosi106@gmail.com>
2023-11-06 13:22:06 +0200

    added task 3 fix

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,6 +2,4 @@
 """instance of an object"""
 def is_kind_of_class(obj, a_class):
     """check instance of an object"""
-    if isinstance(obj, a_class):
-        return True
-    return False
+    return isinstance(obj, a_class) and type(obj) != a_class
