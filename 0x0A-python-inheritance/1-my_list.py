3ebf0bdecb5aa0e4c2e97e6773fc980e2c9643e8
Tyrese-Mv <musawenkosi106@gmail.com>
2023-11-06 12:20:07 +0200

    added task 1 fix 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Inherits from list"""
 class MyList(list):
+    """instantiate using a super class"""
     def __init__(self):
         super().__init__()
     def print_sorted(self):
+        """sorts a list in ascending order"""
         self.sort()
         print(self)
 
