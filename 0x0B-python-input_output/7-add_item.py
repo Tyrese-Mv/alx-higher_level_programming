fdd4737a5722fc32001254d8912747931e2ac74e
Tyrese-Mv <musawenkosi106@gmail.com>
2023-11-07 14:42:48 +0200

    Added task 7 fix 9

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,12 +2,10 @@
 """task 7"""
 
 
-import os.path
 import sys
-import json
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("5-load_from_json_file").load_from_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 def add_items_to_list(arguments):
