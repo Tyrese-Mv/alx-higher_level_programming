224a0fbfdab91ed62a01f737e1619ff2dc7619ca
Tyrese-Mv <musawenkosi106@gmail.com>
2023-11-07 14:15:02 +0200

    Added task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""task 7"""
+
+
+import os.path
+import sys
+import json
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("5-save_to_json_file").load_from_json_file
+
+
+arguments = []
+if os.path.exists('add_item.json') and os.path.getsize('add_item.json') != 0:
+    arguments = load_from_json_file('add_item.json')
+if len(sys.argv) > 1:
+    for idx in range(1, len(sys.argv):
+        arguments.append(sys.argv[idx])
+save_to_json_file(arguments, 'add_item.json')
