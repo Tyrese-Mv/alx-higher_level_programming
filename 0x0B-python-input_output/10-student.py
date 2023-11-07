61e39da5f003722336edf0eebe42bd87b368276d
Tyrese-Mv <musawenkosi106@gmail.com>
2023-11-07 15:40:38 +0200

    added task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -10,6 +10,10 @@ class Student:
         self.last_name = last_name
         self.age = age
 
+    def to_json(self):
+        """attribute"""
+        return self.__dict__
+
     def to_json(self, attrs=None):
 
     """instance function
