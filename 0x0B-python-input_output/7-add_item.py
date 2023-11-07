76f82fa98d70bc172d3d9318d7724c302aff57a3
Tyrese-Mv <musawenkosi106@gmail.com>
2023-11-07 14:27:00 +0200

    Added task 7 fix 4

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,14 +6,17 @@ import os.path
 import sys
 import json
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("5-save_to_json_file").load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__("5-load_from_json_file").load_from_json_file
 
 
-arguments = []
-if os.path.exists('add_item.json') and os.path.getsize('add_item.json') != 0:
-    arguments = load_from_json_file('add_item.json')
-if len(sys.argv) > 1:
-    for idx in range(1, len(sys.argv):
-        arguments.append(sys.argv[idx])
-save_to_json_file(arguments, 'add_item.json')
+    arguments = []
+    if os.path.exists('add_item.json') and os.path.getsize('add_item.json') != 0:
+        arguments = load_from_json_file('add_item.json')
+
+    if len(sys.argv) > 1:
+        for idx in range(1, len(sys.argv):
+                arguments.append(str(sys.argv[idx]))
+
+    save_to_json_file(arguments, 'add_item.json')
