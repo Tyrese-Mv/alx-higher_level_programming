064f5963ff7e6eee109854f6fac90f538c1bd125
Tyrese-Mv <musawenkosi106@gmail.com>
2024-01-08 10:13:41 +0200

    added task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const Arguments = process.argv
+const numArguments = Arguments.length - 2;
+
+if (numArguments === 0){
+    console.log('No argument');
+}
+else{
+    console.log('Argument found');
+}
