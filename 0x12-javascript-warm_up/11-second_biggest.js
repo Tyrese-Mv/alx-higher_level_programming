fcfc56f6aa43b5d113f32380c1a59f82c8f5d060
Tyrese-Mv <musawenkosi106@gmail.com>
2024-01-08 15:08:40 +0200

    fixed task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,13 @@
 #!/usr/bin/node
 
-const Arguments = process.argv;
-const argLength = Arguments.length;
+const Arguments = process.argv.slice(2);
+const myArr = Arguments.map(Arguments => parseInt(Arguments));
+const sorted = myArr.sort(function (a, b) {
+  return b - a;
+});
 
-const myArr = [];
-if (argLength === 2) {
+if (sorted.length < 2) {
   console.log('0');
-} else if (argLength === 3) {
-  console.log('1');
 } else {
-  for (let i = 2; i < argLength; i++) {
-    myArr.push(parseInt(Arguments[i]));
-  }
-  myArr.sort(function (a, b) {
-    return b - a;
-  });
-  console.log(myArr[1]);
+  console.log(sorted[1]);
 }
