f8ff770cc6d4c22e8b8914df2a8dc2eb7b0f7c22
Tyrese-Mv <musawenkosi106@gmail.com>
2024-01-08 15:13:44 +0200

    fixed task 11 fix 3

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,9 +3,7 @@
 const Arguments = process.argv.slice(2);
 const myArr = Arguments.map(Arguments => parseInt(Arguments));
 const numCheck = myArr.filter(myArr => !isNaN(myArr));
-const sorted = numCheck.sort(function (a, b) {
-  return b - a;
-});
+const sorted = numCheck.sort((a, b) => b - a);
 
 if (sorted.length < 2) {
   console.log('0');
