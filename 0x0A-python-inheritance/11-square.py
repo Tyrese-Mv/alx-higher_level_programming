9e9ac099877e84de10ac6ad711f0a773b640c0b3
Tyrese-Mv <musawenkosi106@gmail.com>
2024-04-15 20:40:39 +0200

    added task 12

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """instantiate child class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return ("[Square] {}/{}".format(self.__size, self.__size))
+
+    def area(self):
+        return self.__size * self.__size
