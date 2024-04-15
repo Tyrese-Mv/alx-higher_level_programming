77d215011f0510fe75a3e2bb8ee682c75037a416
Tyrese-Mv <musawenkosi106@gmail.com>
2024-04-15 20:39:51 +0200

    added task 7

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Geometry class"""
+
+
+class BaseGeometry:
+    """create empty geometry class"""
+
+    def area(self):
+        """raises exception"""
+        raise Exception("area() is not implemented")
