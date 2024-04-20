13f609145815ab553e7537aea976a561af9ca946
Tyrese-Mv <musawenkosi106@gmail.com>
2024-04-20 15:54:31 +0200

    added task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,16 +7,18 @@ class Rectangle(Base):
     """Rectangle class"""
     
     def __init__(self, width, height, x=0, y=0, id=None):
+        
         super().__init__(id)
+        Rectangle.__validation(self, width=width, height=height, x=x, y=y)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        
+
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -24,12 +26,12 @@ class Rectangle(Base):
         elif int(value) >= 0:
             self.__width = value
         else:
-            raise ValueError("width must be >= 0")
-    
+            raise ValueError("width must be > 0")
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -37,12 +39,12 @@ class Rectangle(Base):
         elif int(value) >= 0:
             self.__height = value
         else:
-            raise ValueError("height must be >= 0")
-    
+            raise ValueError("height must be > 0")
+
     @property
     def x(self):
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         if not isinstance(value, int):
@@ -51,11 +53,11 @@ class Rectangle(Base):
             self.__x = value
         else:
             raise ValueError("x must be >= 0")
-    
+
     @property
     def y(self):
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         if not isinstance(value, int):
@@ -64,5 +66,22 @@ class Rectangle(Base):
             self.__y = value
         else:
             raise ValueError("y must be >= 0")
-    
-    
+
+    def __validation(self, **kwargs):
+        """Validates the instance attributes
+        Raises:
+            TypeError: <name of the attribute> must be an integer
+            ValueError: <name of the attribute> must be > 0
+            ValueError: <name of the attribute> must be >= 0
+        """
+
+        for key, val in kwargs.items():
+            if not isinstance(val, int):
+                raise TypeError("{} must be an integer".format(key))
+            if key == "width" or key == "height":
+                if int(val) <= 0:
+                    raise ValueError("{} must be > 0".format(key))
+            if key == "x" or key == "y":
+                if int(val) < 0:
+                    raise ValueError("{} must be >= 0".format(key))
+       
