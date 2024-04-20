ae209fc7cb3ee1190e4b97faf975221e0eab0653
Tyrese-Mv <musawenkosi106@gmail.com>
2024-04-20 14:32:45 +0200

    added task 2

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,8 +7,8 @@ class Base:
     __nb_objects = 0
     
     def __init__(self, id=None):
-        __nb_objects += 1
+        Base.__nb_objects += 1
         if id is not None:
             self.id = id
         else:
-            self.id = __nb_objects
+            self.id = Base.__nb_objects
