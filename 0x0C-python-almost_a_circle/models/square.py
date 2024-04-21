b88b5004e13cb4beabc52087d7527aded560e4b0
Tyrese-Mv <musawenkosi106@gmail.com>
2024-04-21 11:37:37 +0200

    added task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,3 +19,27 @@ class Square(Rectangle):
 
     def __str__(self):
         return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__, self.id, self.x, self.y, self.width)
+    
+    def update(self, *args, **kwargs):
+        if (args and len(args) > 0):
+            for index in range(len(args)):
+                match index:
+                    case 0:
+                        self.id = args[index]
+                    case 1:
+                        self.size = args[index]
+                    case 2:
+                        self.x = args[index]
+                    case 3:
+                        self.y = args[index]
+        else:
+            for key, val in kwargs.items():
+                match key:
+                    case "id":
+                        self.id = val
+                    case "size":
+                        self.size = val
+                    case "x":
+                        self.x = val
+                    case "y":
+                        self.y = val
