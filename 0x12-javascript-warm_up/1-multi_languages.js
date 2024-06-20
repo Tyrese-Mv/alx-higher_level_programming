7bee3bf85c4b9a1df766c52fc9cb7e11eb3188b7
Tyrese-Mv <musawenkosi106@gmail.com>
2024-06-20 21:22:01 +0200

    added task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const FirstVar = 'C is fun';
+const secondVar = 'Python is cool';
+const thirdVar = 'JavaScript is amazing';
+
+console.log(FirstVar + '\n' + secondVar + '\n' + thirdVar);
