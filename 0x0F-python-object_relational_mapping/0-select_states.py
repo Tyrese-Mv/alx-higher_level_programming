3b98279acc15e5cebc659d12edbd756ad1186aff
Tyrese-Mv <musawenkosi106@gmail.com>
2024-06-22 11:19:48 +0200

    added task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""select states"""
+import MySQLdb
+import sys
+
+
+def list_states(username, password, db_name):
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+        )
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states ORDER BY states.id ASC"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    username, password, db_name = sys.argv[1:4]
+
+    list_states(username, password, db_name)
