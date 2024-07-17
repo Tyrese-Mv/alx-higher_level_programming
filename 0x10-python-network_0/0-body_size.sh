6019925508e999c38ab36b7c46b95e6454bcdc18
Tyrese-Mv <musawenkosi106@gmail.com>
2024-07-17 22:16:53 +0200

    mod task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
+#!/usr/bin/python3
 # Check if URL is provided
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a request to the URL and display the size of the body in bytes
 curl -s "$1" -o /dev/null -w '%{size_download}\n'
