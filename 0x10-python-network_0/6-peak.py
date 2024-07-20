2cb2c634c4c87fff9f40fa7afc7ebeeacbe3cabf
mmvuna023 <mmvuna023@student.wethinkcode.co.za>
2024-07-20 11:34:18 +0200

    added python script task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Technical interview preparation:"""
+
+
+def find_peak(list_of_integers): 
+    """function that finds a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers (list): list of integers
+
+    Returns:
+        int: largest number
+    """
+    if list_of_integers is None:
+        return None
+    
+    return max(list_of_integers)
