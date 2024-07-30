fca2ec4fcf0f4fe20c771db534b91587a035b5f0
Tyrese-Mv <musawenkosi106@gmail.com>
2024-07-30 18:53:42 +0200

    added task 2

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+//script that writes a string to a file.
+const fs = require('fs');
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+if (!filePath || !content) {
+	console.error('Please provide a file path and a string to write as arguments.');
+	process.exit(1);
+}
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	console.log('Content written successfully!');
+});
+
